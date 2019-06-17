cc7c3bbb58ff1adb1a0e9ee7c06e7bc609bda57d
Computer Lab <complab@asms.net>
2019-06-17 09:15:05 -0500

    Commit

--- app/src/main/java/com/example/lynn/first/MyListener.java
+++ app/src/main/java/com/example/lynn/first/MyListener.java
@@ -1,12 +1,25 @@
 package com.example.lynn.first;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.view.View;
 
 import static com.example.lynn.first.MainActivity.*;
 
 public class MyListener implements View.OnClickListener {
+
+    public int color() {
+        int red = (int)(256*Math.random());
+        int green = (int)(256*Math.random());
+        int blue = (int)(256*Math.random());
+
+        return(Color.argb(255,red,green,blue));
+    }
     @Override
     public void onClick(View view) {
+        int[] colors = {color(),color()};
+
+        GradientDrawable drawable = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP)
 
     }
 }
